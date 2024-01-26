3ffe7c4c8aea76adf3f7c9a728d757e2bbad00ff
haha58 <1461089222@qq.com>
2024-01-26 23:17:06 +0800

    🚀新增: 增加更新用户登录

--- src/controller/users/index.js
+++ src/controller/users/index.js
@@ -1,6 +1,9 @@
 const sendVeriCode = require('./sendVeriCode')
 const loginByPhoneNumber = require('./loginByPhoneNumber')
+const updateUserInfo = require('./updateUserInfo')
+
 module.exports = {
   sendVeriCode,
-  loginByPhoneNumber
+  loginByPhoneNumber,
+  updateUserInfo
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,7 @@
 const router = require('koa-router')()
 const genValidator = require('../middlewares/genValidator')
-const {phoneNumberSchema,phoneNumberVeriCodeSchema} = require('../utils/validator/users')
-const {sendVeriCode,loginByPhoneNumber} = require('../controller/users/index')
+const { phoneNumberSchema, phoneNumberVeriCodeSchema, userInfoSchema } = require('../utils/validator/users')
+const { sendVeriCode, loginByPhoneNumber, updateUserInfo } = require('../controller/users/index')
 const loginCheck = require('../middlewares/loginCheck')
 const { SuccessRes } = require('../res-model/index')
 // 路由前缀
@@ -28,6 +28,13 @@ router.post('/loginByPhoneNumber', genValidator(phoneNumberVeriCodeSchema), asyn
 router.get('/getUserInfo', loginCheck, async ctx => {
     // 经过了 loginCheck ，用户信息在 ctx.userInfo 中
     ctx.body = new SuccessRes(ctx.userInfo)
+})
+
+// 修改用户信息
+router.patch('/updateUserInfo', loginCheck, genValidator(userInfoSchema), async ctx => {
+    // 经过了 loginCheck ，用户信息在 ctx.userInfo 中
+    const res = await updateUserInfo(ctx.userInfo, ctx.request.body)
+    ctx.body = res
 })
 
 module.exports = router
